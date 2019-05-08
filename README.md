44a63cc2a13e4d76f76548b7cf5273ecd10c7c46
Rizal Hermawan <rizalhrm24@gmail.com>
2019-05-08 13:40:47 +0700

    learn api with mongoDB and express

---
+++ README.md
@@ -0,0 +1,47 @@
+## Hasil dari Route Get dengan Relasi Document Referenced
+
+```
+[
+    {
+        "_id": "5cd25fc1173df42af46ecd28",
+        "id": "4",
+        "artistname": "Rizal Hermawan",
+        "band_members": [
+            {
+                "_id": "5cd13060173df42af46ecd1e",
+                "singer_name": "GHI",
+                "instrument": [
+                    "Accordion",
+                    "Jaw Harps",
+                    "Keyboards"
+                ],
+                "artist_id": "4"
+            },
+            {
+                "_id": "5cd146cf173df42af46ecd1f",
+                "singer_name": "ABC",
+                "instrument": [
+                    "Banjo",
+                    "Cello"
+                ],
+                "artist_id": "4"
+            }
+        ]
+    },
+    {
+        "_id": "5cd2645c173df42af46ecd29",
+        "id": "5",
+        "artistname": "Suparman",
+        "band_members": [
+            {
+                "_id": "5cd15068173df42af46ecd21",
+                "singer_name": "Gita",
+                "instrument": [
+                    "OK"
+                ],
+                "artist_id": "5"
+            }
+        ]
+    }
+]
+```
---
+++ index.js
@@ -0,0 +1,175 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const Joi = require('joi');
+const assert = require('assert');
+const db = require("./db");
+const collection = "singers";
+const app = express();
+
+// schema used for data validation for our document
+const schema = Joi.object().keys({
+    id: Joi.number().integer().required().min(1).max(1000),
+    artistname : Joi.string().required()
+});
+
+// parses json data sent to us by the user 
+app.use(bodyParser.json());
+
+// serve static html file to user
+app.get('/',(res)=>{
+    res.send("welcome");
+});
+
+// read
+app.get('/singers',(req,res)=>{
+    // get all documents within our collection
+    // send back to user as json
+    db.getDB().collection(collection).aggregate(
+        [ { "$lookup": 
+              {
+                  "from": "instruments",
+                  "localField" : "id",
+                  "foreignField": "artist_id",
+                  "as": "band_members"
+              }
+          }
+        ],
+        (err, cursor) => {
+          assert.equal(err, null);
+  
+          cursor.toArray(function(err, results) {
+            if (err) {
+                res.status(400).send({'error': err})
+            }
+            if (results === undefined || results.length === 0) {
+                res.status(400).send({'error':'No documents in database'})
+            } else {
+                res.status(200).send(results)
+            }
+          });
+        }
+    );
+});
+
+// get by Id
+app.get('/singer/:id', (req, res) => {
+    let id = req.params.id
+    db.getDB().collection(collection).aggregate(
+        [ { "$lookup": 
+              {
+                  "from": "instruments",
+                  "localField" : "id",
+                  "foreignField": "artist_id",
+                  "as": "band_members"
+              }
+          },
+          { "$match" : 
+              { 
+                  "_id" : db.getPrimaryKey(id)
+              } 
+          }
+        ],
+        (err, cursor) => {
+          assert.equal(err, null);
+  
+          cursor.toArray(function(err, result) {
+            if (err) {
+                res.status(400).send({'error': err})
+            }
+            if (result === undefined) {
+                res.status(400).send({'error':'No documents in database'})
+            } else {
+                res.status(200).send(result)
+            }
+          });
+        }
+    );
+})
+
+//create
+app.post('/singer',(req,res,next)=>{
+    // Document to be inserted
+    const userInput = req.body;
+
+    // Validate document
+    // If document is invalid pass to error middleware
+    // else insert document within collection
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to insert Document");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.status(200).send({result : result, document : result.ops[0],msg : "Successfully inserted Data!!!",error : null});
+            });
+        }
+    })    
+});
+
+// update
+app.put('/singer/:id',(req,res)=>{
+    // Primary Key of Document we wish to update
+    const id = req.params.id;
+    // Document used to update
+    const userInput = req.body;
+    // Find Document By ID and Update
+    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(id)},{$set : {id : userInput.id, artistname : userInput.artistname}},{returnOriginal : false},(err,result)=>{
+        if (err) {
+            res.status(400).send({'error': err})
+        }
+        else{
+            res.status(200).send(result);
+        }      
+    });
+});
+
+//delete
+app.delete('/singer/:id',(req,res)=>{
+    // Primary Key of Document
+    const id = req.params.id;
+    // Find Document By ID and delete document from record
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(id)},(err,result)=>{
+        if (err) {
+            res.status(400).send({'error': err})
+        }
+        else{
+            res.status(200).send(result);
+        } 
+    });
+});
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
+})
+
+
+db.connect((err)=>{
+    // If err unable to connect to database
+    // End application
+    if(err){
+        console.log('unable to connect to database');
+        process.exit(1);
+    }
+    // Successfully connected to database
+    // Start up our Express Application
+    // And listen for Request
+    else{
+        app.listen(3000,()=>{
+            console.log('connected to database, app listening on port 3000');
+        });
+    }
+});
